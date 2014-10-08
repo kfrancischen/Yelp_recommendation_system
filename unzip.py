60f937c9cbf283cf5d464f647747f72f2de4ca7d
kfrancischen <kfrancischen@gmail.com>
2014-10-08 11:19:03 -0700

    First commit
    unzip.py is used to read the compressed data set. Change name in
    “all.txt.gz”

---
+++ unzip.py
@@ -0,0 +1,20 @@
+import gzip
+import simplejson
+
+def parse(filename):
+  f = gzip.open(filename, 'r')
+  entry = {}
+  for l in f:
+    l = l.strip()
+    colonPos = l.find(':')
+    if colonPos == -1:
+      yield entry
+      entry = {}
+      continue
+    eName = l[:colonPos]
+    rest = l[colonPos+2:]
+    entry[eName] = rest
+  yield entry
+
+for e in parse("all.txt.gz"):
+  print simplejson.dumps(e)
