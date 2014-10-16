884159304c57cbfbbea7dc1b7fb03d007e533442
kfrancischen <kfrancischen@gmail.com>
2014-10-15 21:47:17 -0700

    reading json files

---
+++ dataAccess.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+# Open the Yelp dataset
+#fileDir= '/Users/jennifer/yelp_dataset_challenge_academic_dataset 2/'
+fileName = 'yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_checkin.json'
+filePath = fileName
+maxlines = 15
+
+import json
+
+with open(filePath) as f:
+	counter = 0
+	for line in f:
+		if counter > maxlines:
+			break
+		while True:
+			counter +=1
+			try:
+					json_data = json.loads(line) 
+					print(json_data)
+					break
+			except ValueError:
+					line += next(f)
+
+f.close( )
+	
