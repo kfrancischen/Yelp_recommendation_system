121e633567c35d19632a359ceb948ca3391db9af
kfrancischen <kfrancischen@gmail.com>
2014-10-31 23:19:30 -0700

    add some files

--- reviewAnalysis.py
+++ reviewAnalysis.py
@@ -1,4 +1,6 @@
 import convertJson
+import statsVisualize
+import validating 
 
 allRestaurants = convertJson.readRestaurants(['business_id','name','stars','review_count'])
 
@@ -6,6 +8,17 @@ goodRestaurants = convertJson.readGoodRestaurants(800)
 
 trueRating = convertJson.trueRatings(goodRestaurants)
 #print trueRating
-textualRating = convertJson.textualRatings(goodRestaurants)
+alpha = [0.2*(i+1) for i in range(10)]
+scores = []
+bins = [1,1.5,2,2.5,3,3.5,4,4.5,5]
 
-print trueRating, textualRating
+for i in range(len(alpha)):
+	textualRating = convertJson.textualRatings(goodRestaurants,alpha[i])
+	scores.append(validating.errorTesting(textualRating, trueRating, bins))
+print scores
+
+#print trueRating, textualRating
+
+#textualRating = convertJson.textualRatings(goodRestaurants,1)
+#statsVisualize.visualizing(textualRating,trueRating,5)
+
---
+++ validating.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+def errorTesting(textualRating, trueRating,bins):
+	textR = textualRating.values()
+	trueR = trueRating.values()
+	textHist, _ = np.histogram(textR, bins)
+	trueHist, _ = np.histogram(trueR, bins)
+	#print textHist,trueHist
+	weights=[]
+	for i in range(len(bins)-1):
+		weights.append((bins[i]+bins[i+1])/2)
+#	print weights
+	textHist = list(textHist)
+	trueHist = list(trueHist)
+#	print textHist,trueHist
+	textVariance = 0
+	trueVariance = 0
+	for i in range(len(textHist)):
+		textVariance += (textHist[i]-np.average(textHist))**2
+		trueVariance += (trueHist[i] -np.average(trueHist))**2
+	score = (trueVariance - textVariance)**2 + (np.average(textHist,weights=weights) - np.average(trueHist,weights=weights))**2	
+	return score
+
+print errorTesting({'a':1,'b':2},{'a':2,'b':1,'c':1},[0.5,1.5,2.5])
